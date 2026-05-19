c5b6902d4b97370590d2fe0d3149be594029b669
Naty <nata.alvial@duocuc.cl>
2026-05-19 04:47:00 -0400

    sin gotrue

--- app/src/main/java/com/example/huertohogardefinitiveedition/supabase/SupabaseConfig.kt
+++ app/src/main/java/com/example/huertohogardefinitiveedition/supabase/SupabaseConfig.kt
@@ -2,13 +2,12 @@ package com.example.huertohogardefinitiveedition.supabase
 
 import io.github.jan.supabase.SupabaseClient
 import io.github.jan.supabase.createSupabaseClient
-import io.github.jan.supabase.gotrue.GoTrue
 import io.github.jan.supabase.postgrest.Postgrest
 
 object SupabaseConfig {
 
     // REEMPLAZAR CON TUS DATOS DE SUPABASE
-    private const val SUPABASE_URL = "https://htseatyrphfyrjmrqtqv.supabase.co/rest/v1/"
+    private const val SUPABASE_URL = "https://htseatyrphfyrjmrqtqv.supabase.co/"
     private const val SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh0c2VhdHlycGhmeXJqbXJxdHF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxNTc2MjMsImV4cCI6MjA5NDczMzYyM30.-DgP4NM3F7Ckd8HwxREck8X8LOkDTTv1ZMGYEvW0FTs"
 
     val client: SupabaseClient = createSupabaseClient(
@@ -16,6 +15,5 @@ object SupabaseConfig {
         supabaseKey = SUPABASE_KEY
     ) {
         install(Postgrest)
-        install(GoTrue)
     }
 }
